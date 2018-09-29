c99024d518fcd1b3c0240cde4d4b5567a7269b19
Stav Hadas <4stav.h@gmail.com>
2018-09-29 14:24:06 +0300

    soceket io setup on server and client

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+
+const publicPath = path.join(__dirname, '../public');
+const port = 3000;
+console.log(publicPath);
+var app = new express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection',(socket) =>{
+    console.log('Connected to host');
+
+});
+server.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+});
